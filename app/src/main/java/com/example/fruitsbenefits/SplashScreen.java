e4f889e4def850b8fb01c357ecf83a54dfef5a22
amidelu <delowar_gtc@yahoo.com>
2019-03-30 00:45:08 +0600

    Initial commit

---
+++ app/src/main/java/com/example/fruitsbenefits/SplashScreen.java
@@ -0,0 +1,26 @@
+package com.example.fruitsbenefits;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    private final int splashLength = 4000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent splashIntent = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(splashIntent);
+                finish();
+            }
+        }, 4000);
+    }
+}
